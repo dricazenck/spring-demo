f3d8a2426777cffab4bc66cefdfdccd82d820a5f
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-09-09 20:41:48 +0200

    add gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
 
 group = "com.wcc.springdemo"
@@ -13,8 +14,13 @@ java {
     }
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
