3505968018901b7d614da9a8f35b5f1156cea300
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2024-09-09 23:06:17 +0200

    bean for object mapper

--- src/main/java/com/wcc/springdemo/demo/config/JacksonConfig.java
+++ src/main/java/com/wcc/springdemo/demo/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package com.wcc.springdemo.demo.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,10 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        
+
         ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
         return objectMapper;
