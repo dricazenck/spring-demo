29ec271192a0ed612a4ac2818ec844b8154b49b3
Nevena Verbic <nevena.verbic@gmail.com>
2024-09-10 14:50:27 +0200

    Add UserController test - post api

--- src/test/java/com/wcc/springdemo/demo/controller/UserControllerTest.java
+++ src/test/java/com/wcc/springdemo/demo/controller/UserControllerTest.java
@@ -1,19 +1,16 @@
 package com.wcc.springdemo.demo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wcc.springdemo.demo.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,17 +19,29 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    public void testGetAllUsers() throws Exception {
+    public void testGetAllUsersOk() throws Exception {
         mockMvc.perform(get("/api/v1/users"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$.[0].username", is("adriana")));
     }
 
-
     @Test
-    void createUser() {
+    void testCreateUserOk() throws Exception {
+        mockMvc.perform(post("/api/v1/user")
+                        .content(objectMapper.writeValueAsString(new User("3", "username", "FirstName", "LastName", "FirstName LastName", "username@email.com")))  // Sending the new user name in the body
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk());
+
+        // Verify that the new user is added to the list via GET request
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$.[2].username", is("username")));;
       }
 
     @Test
