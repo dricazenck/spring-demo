df5d1b78ad275e48fbdfd23690151aa9bfd42946
Nevena Verbic <nevena.verbic@gmail.com>
2024-09-10 19:24:13 +0200

    Update user controller test for post user and get by identifier

--- src/test/java/com/wcc/springdemo/demo/controller/UserControllerTest.java
+++ src/test/java/com/wcc/springdemo/demo/controller/UserControllerTest.java
@@ -24,49 +24,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
-    @MockBean
-    private UserService userService;
+  @MockBean private UserService userService;
 
-    @Test
-    public void testGetAllUsersOk() throws Exception {
-    var users = List.of(new User("1", "username_1", "FirstName_1", "LastName_1", "FirstName_1 LastName_1", "username_1@mail.com"));
+  @Test
+  public void testGetAllUsersOk() throws Exception {
+    var users =
+        List.of(
+            new User(
+                "1",
+                "username_1",
+                "FirstName_1",
+                "LastName_1",
+                "FirstName_1 LastName_1",
+                "username_1@mail.com"));
 
-        when(userService.getAll()).thenReturn(users);
+    when(userService.getAll()).thenReturn(users);
 
-        mockMvc.perform(get("/api/v1/users"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$.[0].username", is("username_1")));
+    mockMvc
+        .perform(get("/api/v1/users"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(1)))
+        .andExpect(jsonPath("$.[0].username", is("username_1")));
+  }
+
+  @Test
+  void testCreateUserOk() throws Exception {
+    var user =
+        new User(
+            "2",
+            "username_2",
+            "FirstName_2",
+            "LastName_2",
+            "FirstName_2 LastName_2",
+            "username_2@mail.com");
+
+    when(userService.addUser(user)).thenReturn(user);
+
+    mockMvc
+        .perform(
+            post("/api/v1/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+        .andExpect(status().isOk());
+    // .andExpect(content().json(userAsString(user)));
+  }
+
+  private String userAsString(final User user) {
+    try {
+      return objectMapper.writeValueAsString(user);
+    } catch (IOException e) {
+      return User.class.toString();
     }
+  }
 
-    @Test
-    void testCreateUserOk() throws Exception {
-        var user = new User("2", "username_2", "FirstName_2", "LastName_2", "FirstName_2 LastName_2", "username_2@mail.com");
+  @Test
+  void testGetUserByUserNameOk() throws Exception {
+    var users =
+        List.of(
+            new User(
+                "1",
+                "username_1",
+                "FirstName_1",
+                "LastName_1",
+                "FirstName_1 LastName_1",
+                "username_1@mail.com"),
+            new User(
+                "2",
+                "username_2",
+                "FirstName_2",
+                "LastName_2",
+                "FirstName_2 LastName_2",
+                "username_2@mail.com"));
 
-        when(userService.addUser(user)).thenReturn(user);
+    when(userService.getAll()).thenReturn(users);
 
-        mockMvc.perform(post("/api/v1/user")
-                        .content(objectMapper.writeValueAsString(user))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json(userAsString(user)));
-        }
-
-    private String userAsString(final User user) {
-        try {
-            return objectMapper.writeValueAsString(user);
-        } catch (IOException e) {
-            return User.class.toString();
-        }
-    }
-
-    @Test
-    void testGetUserByUserNameOk() {
-      }
+    mockMvc
+        .perform(get("/api/v1/user/username_1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.username", is("username_1")));
+  }
 }
