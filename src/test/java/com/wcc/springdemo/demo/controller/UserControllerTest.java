39c4f3d8a423185ddbbc9f5218573d67360eaa40
Nevena Verbic <nevena.verbic@gmail.com>
2024-09-10 11:27:03 +0200

    Add UserController test

---
+++ src/test/java/com/wcc/springdemo/demo/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.wcc.springdemo.demo.controller;
+
+import com.wcc.springdemo.demo.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllUsers() throws Exception {
+        mockMvc.perform(get("/api/v1/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$.[0].username", is("adriana")));
+    }
+
+
+    @Test
+    void createUser() {
+      }
+
+    @Test
+    void getUserById() {
+      }
+
+    @Test
+    void getUserByUserName() {
+      }
+}
