369ef2d291588e8cc43e33a8504323cef2b05b7e
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2025-05-17 16:10:21 +0200

    adjust format

--- src/main/java/com/wcc/springdemo/demo/service/ProductService.java
+++ src/main/java/com/wcc/springdemo/demo/service/ProductService.java
@@ -3,59 +3,59 @@ package com.wcc.springdemo.demo.service;
 import com.wcc.springdemo.demo.domain.Product;
 import com.wcc.springdemo.demo.exception.ProductIdDuplicatedException;
 import com.wcc.springdemo.demo.exception.ProductNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-    private final List<Product> products = new ArrayList<>();
+  private final List<Product> products = new ArrayList<>();
 
-    public List<Product> getAllProducts() {
-        return products;
+  public List<Product> getAllProducts() {
+    return products;
+  }
+
+  public Product createProduct(Product product) {
+
+    if (getProductById(product.id()) != null) {
+      throw new ProductIdDuplicatedException(product.id());
     }
 
-    public Product createProduct(Product product) {
+    products.add(product);
 
-        if (getProductById(product.id()) != null) {
-            throw new ProductIdDuplicatedException(product.id());
-        }
+    return product;
+  }
 
-        products.add(product);
-
-        return product;
+  public Boolean deleteProduct(String id) {
+    Product product = getProductById(id);
+    if (product != null) {
+      return products.remove(product);
     }
 
-    public Boolean deleteProduct(String id) {
-        Product product = getProductById(id);
-        if (product != null) {
-            return products.remove(product);
-        }
+    return false;
+  }
 
-        return false;
+  public Product updateProduct(Product product) {
+    if (getProductById(product.id()) == null) {
+      throw new ProductNotFoundException(product.id());
     }
 
-    public Product updateProduct(Product product) {
-        if (getProductById(product.id()) == null) {
-            throw new ProductNotFoundException(product.id());
-        }
+    products.add(product);
 
-        products.add(product);
+    return product;
+  }
 
-        return product;
-    }
+  public Product getProductById(String id) {
+    return products.stream()
+        .filter(user -> user.id().equalsIgnoreCase(id))
+        .findFirst()
+        .orElse(null);
+  }
 
-    public Product getProductById( String id) {
-        return products.stream().filter(user -> user.id().equalsIgnoreCase(id)).findFirst().orElse(null);
-    }
-
-    public Product getProductByName( String name) {
-        return products.stream().filter(user -> user.name().equalsIgnoreCase(name))
-                .findFirst()
-                .orElseThrow(() -> new ProductNotFoundException(name));
-
-    }
+  public Product getProductByName(String name) {
+    return products.stream()
+        .filter(user -> user.name().equalsIgnoreCase(name))
+        .findFirst()
+        .orElseThrow(() -> new ProductNotFoundException(name));
+  }
 }
